0f6188eeb524667375a0e07a765916bde8012462
Javier Moreno <email@javier-moreno.com>
2022-06-07 19:53:42 +0000

    Activated mysql profile

--- scripts/run.sh
+++ scripts/run.sh
@@ -4,4 +4,4 @@ echo Using RDS endpoint $RDS.
 sed -i "s~localhost:3306~$RDS:3306~g; s~username>petclinic</jdbc~username>petclinic</jdbc~g; s~password>petclinic</jdbc~password>p3tcl1n1c</jdbc~g" pom.xml
 
 echo Starting application.
-mvn jetty:run-war  -DskipTests=true
+mvn jetty:run-war -P MySQL -DskipTests=true
